1b9e0d2a12cc582159fce3c2dd8ad9399f88831e
Jan Šťástka <jan.stastka@pslib.cz>
2019-12-29 16:13:42 +0100

    29.12

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 			<footer class="footer" role="contentinfo">
 
 				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> | <?php _e('By', ''); ?> <a href="//jsweb.pslib.cloud" title="Jan Šťástka">Jan Šťástka</a>
+					&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> <?php _e('|', ''); ?> 
+					<a href="//jsweb.pslib.cloud" title="Jan Šťástka">By Jan Šťástka</a>
 				</p>
 
 			</footer>
