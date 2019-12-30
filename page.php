384051e4a57c24ae90d617a2f6566cbecc682286
Jan Šťástka <jan.stastka@pslib.cz>
2019-12-30 15:14:44 +0100

    30.12

--- page.php
+++ page.php
@@ -13,11 +13,7 @@
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-			<article>
-
-				<?php the_content(); ?>
-
-			</article>
+			<?php the_content(); ?>
 
 		<?php endwhile; endif; ?>
 
